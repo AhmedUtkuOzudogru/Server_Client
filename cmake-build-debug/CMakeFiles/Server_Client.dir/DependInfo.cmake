d8472ee1915ccac981bd09eb8e178745114cb096
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2024-07-18 23:26:42 +0300

    Directory Change

---
+++ cmake-build-debug/CMakeFiles/Server_Client.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ahmed/CLionProjects/Serrver-Client/Client.cpp" "CMakeFiles/Server_Client.dir/Client.cpp.obj" "gcc" "CMakeFiles/Server_Client.dir/Client.cpp.obj.d"
+  "C:/Users/ahmed/CLionProjects/Serrver-Client/Server.cpp" "CMakeFiles/Server_Client.dir/Server.cpp.obj" "gcc" "CMakeFiles/Server_Client.dir/Server.cpp.obj.d"
+  "C:/Users/ahmed/CLionProjects/Serrver-Client/main.cpp" "CMakeFiles/Server_Client.dir/main.cpp.obj" "gcc" "CMakeFiles/Server_Client.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Server_Client.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server_Client.dir/Client.cpp.obj"
+  "CMakeFiles/Server_Client.dir/Client.cpp.obj.d"
+  "CMakeFiles/Server_Client.dir/Server.cpp.obj"
+  "CMakeFiles/Server_Client.dir/Server.cpp.obj.d"
+  "CMakeFiles/Server_Client.dir/main.cpp.obj"
+  "CMakeFiles/Server_Client.dir/main.cpp.obj.d"
+  "Server_Client.exe"
+  "Server_Client.exe.manifest"
+  "Server_Client.pdb"
+  "libServer_Client.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Server_Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
